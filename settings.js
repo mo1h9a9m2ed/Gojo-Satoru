238ccb2b097d58862578e0f7b0f6e323a8c7fde5
mo1h9a9m2ed <99542588+mo1h9a9m2ed@users.noreply.github.com>
2022-07-01 08:56:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+212678731209']
+global.premium = ['+212678731209']
+global.ownernomer = '+212678731209'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
